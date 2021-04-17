469f3fadca80449f301794eba1797aea1930afd2
Viral parmar <31970912+vral-parmar@users.noreply.github.com>
2021-04-17 11:31:29 +0530

    Create cred.py

---
+++ cred.py
@@ -0,0 +1,10 @@
+# cred.py file
+#Author  : 	Viral Parmar   
+#Github  : 	https://github.com/vral-parmar
+#Twitter : 	https://twitter.com/vral_parmar
+#live Bot:	https://twitter.com/TaelynCorb
+
+consumer_key = 'Your Consumer Key'
+consumer_secret = 'Your Consumer Secret'
+access_token = 'Your Access Token'
+access_token_secret = 'Your Access Token Secret'
